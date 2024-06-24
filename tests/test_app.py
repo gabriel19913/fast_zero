8d7f7ae5541cc96210695008460aec0407b94e99
Gabriel Fonseca <gabrielfonseca91@hotmail.com>
2024-06-24 20:53:09 -0300

    Adiciona arquivo de schemas

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,9 @@ from fast_zero.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app) # Arrange (organização)
+    client = TestClient(app)  # Arrange (organização)
 
-    response = client.get('/') # Act (ação)
+    response = client.get('/')  # Act (ação)
 
-    assert response.status_code == HTTPStatus.OK # Assert (garantir)
+    assert response.status_code == HTTPStatus.OK  # Assert (garantir)
     assert response.json() == {'message': 'Olá mundo!'}
